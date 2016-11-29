928271d349405b4cac4e237091cf4167d6af93ef
Tenkay <tenkay.tenkai@gmail.com>
2016-11-28 22:08:42 -0300

    Commented code and added prefix

--- mybot.js
+++ mybot.js
@@ -1,14 +1,29 @@
 var Discord = require("discord.js");
 var bot = new Discord.Client();
 
-bot.on("message", msg => {
-    if (msg.content.startsWith("ping")) {
-        msg.channel.sendMessage("pong!");
-    }
-});
-
 bot.on('ready', () => {
-  console.log('I am ready!');
+	console.log('I am ready!');
 });
 
+bot.on("message", msg => {
+	// Sets prefix
+	let prefix = "!";
+
+	// Stop if message doesn't have the prefix
+	if(!msg.content.startsWith(prefix)) return;
+
+	// Stop if the message's author is a bot
+	if(msg.author.bot) return;
+
+	// Commands
+	if (msg.content.startsWith(prefix + "ping")) {
+		msg.channel.sendMessage("pong!");
+	}
+	else if (msg.content.startsWith(prefix + "bestgirl")){
+		msg.channel.sendMessage("Its me!");
+	}
+});
+// Catches errors
+bot.on('error', e => { console.error(e); });
+// Sets Token
 bot.login("TokenHere");
