2b8609aa9fa4445f5cae47004304a6a625ebf79d
Tenkay <tenkay.tenkai@gmail.com>
2016-12-21 20:51:12 -0300

    Renamed some commands. Added prune & mention

--- mybot.js
+++ mybot.js
@@ -8,20 +8,22 @@ bot.on('ready', () => {
 let prefix = "!";
 var response = {
 	"ping" : "pong!",
-	"help" : "The current commands are:\n\n**!ping** - Pong!\n\n**!bestgirl** - You know who that is!\n\n**!setAvatar** - Sets the bot's avatar.\n\n**!myAvatar** - Shows your avatar's URL\n\n**!setName** - Changes the bot's username.",
+	"help" : "The current commands are:\n\n**!ping** - Pong!\n\n**!bestgirl** - You know who that is!\n\n**!setavatar** - Sets the bot's avatar.\n\n**!myavatar** - Shows your avatar's URL\n\n**!setname** - Changes the bot's username.\n\n**!mention** - Mentions an user\n\n**!vc** - Joins VC and plays something\n\n**!prune** - Prunes chat messages.",
 	"bestgirl" : "Its me!",
 	
 };
 
 bot.on('message', (message) => {
-	if (!message.content.startsWith(prefix)) return;
+
 	var content = message.content.substr(1);
+	let args = message.content.split(" ");
+	
+	if (!message.content.startsWith(prefix) || (message.author.id != 110836228391211008 )) return;
 	if (content in response) {
 		message.channel.sendMessage(response[content]);
 }
 	//Set Avatar
 	if (message.content.startsWith(prefix + "setAvatar")){
-		let args = message.content.split(" ");
 		bot.user.setAvatar(args[1]);
 		setTimeout(function(){
 			console.log('New avatar set!');
@@ -30,20 +32,42 @@ bot.on('message', (message) => {
 	}
 
 	// User's Avatar Function
-	if (message.content === (prefix + "myAvatar")) {
+	if (message.content === (prefix + "myavatar")) {
 		message.channel.sendMessage(message.author.avatarURL);
 	}
 
 	// Username change Function
-	if (message.content.startsWith (prefix + "setName")) {
-		let args = message.content.split(" ");
+	if (message.content.startsWith (prefix + "setname")) {
 		bot.user.setUsername(args[1]);
 		setTimeout(function() {
 			message.channel.sendMessage("Username set!");
 		}, 2000);
 	}
 
-	// Join Vc and play .mp3
+	// Mentions an user
+	if (message.content.startsWith (prefix + "mention")) {
+		if (args.length == 3){
+			mentionedUser = bot.users.find('username', args[1]+" "+args[2]);
+			message.channel.sendMessage(mentionedUser);
+		}
+		if (args.length == 4){
+			mentionedUser = bot.users.find('username', args[1]+" "+args[2]+" "+args[3]);
+			message.channel.sendMessage(mentionedUser);
+		}
+		else {
+			mentionedUser = bot.users.find('username', args[1]);
+			message.channel.sendMessage(mentionedUser);
+		}
+	}
+
+	// Prunes messages
+	if(message.content.startsWith(prefix+"prune")) {
+		let messagecount = parseInt(args[1]);
+		message.channel.fetchMessages({limit: 100})
+		message.channel.bulkDelete(messagecount + 1);
+	}
+
+	// Joins Vc and plays .mp3
 	if (message.content ===(prefix + "vc")){
 		channel = message.member.voiceChannel;
 		channel.join()
@@ -52,7 +76,7 @@ bot.on('message', (message) => {
 		return connection.playFile('song.mp3');
 	})
 		.catch(console.error);
-	}
+	} 
 })
 
 // Catches errors
