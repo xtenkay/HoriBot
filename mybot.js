53c0f04f3c393f077b40ad30f6f19499d5612371
Tenkay <tenkay.tenkai@gmail.com>
2016-12-20 22:46:54 -0300

    Added a !help and others like username and avatar.

--- mybot.js
+++ mybot.js
@@ -15,15 +15,48 @@ bot.on("message", msg => {
 	// Stop if the message's author is a bot
 	if(msg.author.bot) return;
 
-	// Commands
-	if (msg.content.startsWith(prefix + "ping")) {
+	//Help Function
+	// This lists all commands available.
+	if (msg.content === (prefix + "help")){
+		msg.channel.sendMessage(
+			"The current commands are:\n\n**!ping** - Pong!\n\n**!bestgirl** - You know who that is!\n\n**!setAvatar** - Sets the bot's avatar.\n\n**!myAvatar** - Shows your avatar's URL\n\n**!setName** - Changes the bot's username."
+		);
+	}
+
+	//Ping Function
+	if(msg.content === (prefix + "ping")){
 		msg.channel.sendMessage("pong!");
 	}
-	else if (msg.content.startsWith(prefix + "bestgirl")){
+	
+	//Best Girl Function
+	if (msg.content === (prefix + "bestgirl")){
 		msg.channel.sendMessage("Its me!");
 	}
+
+	// Set Avatar Function
+	if (msg.content.startsWith(prefix + "setAvatar")){
+		let args = msg.content.split(" ");
+		bot.user.setAvatar(args[1]);
+		console.log('New avatar set!');
+		msg.channel.sendMessage("Avatar set!");
+		
+	}
+
+	// User's Avatar Function
+	if (msg.content === (prefix + "myAvatar")) {
+		msg.channel.sendMessage(msg.author.avatarURL);
+	}
+
+	// Username change Function
+	if (msg.content.startsWith (prefix + "setName")) {
+		let args = msg.content.split(" ");
+		bot.user.setUsername(args[1]);
+		msg.channel.sendMessage("Username set!");
+
+	}
 });
+
 // Catches errors
 bot.on('error', e => { console.error(e); });
 // Sets Token
-bot.login("TokenHere");
+bot.login("MjUyOTUzMTYxMDI1NzE2MjI1.Cztkvw.z0ypnOJyzVlqMW_4eRRqk09fjpc");
